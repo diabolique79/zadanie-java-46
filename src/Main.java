b64a1b6144814e5a721fee5dedb41786a496d348
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-13 21:33:15 +0200

    Rozwiązanie zadania dodatkowego

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+    public class Main {
+
+        public static void main(String[] args) {
+            System.out.print((char)75);
+            System.out.print((char)79);
+            System.out.print((char)84);
+        }
+    }
+
